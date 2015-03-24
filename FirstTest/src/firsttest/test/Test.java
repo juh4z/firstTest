77094499091d95db0622fc2c3186ac200167d073
noponenj <noponenj@PORI257-012-SPC.intra.tut.fi>
2015-03-24 10:28:44 +0200

    Committoidaan muutokset

---
+++ FirstTest/src/firsttest/test/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package firsttest.test;
+
+/**
+ * @author noponenj
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("FirstTest!!!");
+
+	}
+
+}
